15a500c9aeaa6848cbf3e6219b8b4d28fb955d19
unknown <likhonsorkar02@yahoo.com>
2023-05-17 21:46:54 +0600

    blog post + sidebar + more fuction for post

--- functions.php
+++ functions.php
@@ -4,8 +4,23 @@
 */
 
 
-// Theme Title
+// Theme Supports
 add_theme_support('title-tag');
+
+add_theme_support('post-thumbnails', array( 'post', 'page' ) );
+add_image_size( 'my-custom-size', 970, 350, true );
+
+
+// read more
+function nexus_custom_excerpt_more( $more ) {
+  return '<br> <a href="' . get_permalink( get_the_ID() ) . '" class="readmorebtn">Read More</a>';
+}
+add_filter( 'excerpt_more', 'nexus_custom_excerpt_more' );
+
+function nexus_excerpt_lenght($length){
+  return 40;
+}
+add_filter('excerpt_length', 'nexus_excerpt_lenght', 999);
 
 
 // Theme CSS and jQuery File calling
@@ -23,7 +38,28 @@ function nexus_css_js(){
 }
 add_action('wp_enqueue_scripts', 'nexus_css_js');
 
+// Theme Customizer Run
 
+function nexus_customize_register($wp_customize){
+    // footer copyright text customize start
+    $wp_customize->add_section('nexus_footer', array(
+      'title' => __('Footer Settings', 'nexus'),
+    ));
+    $wp_customize->add_setting('nexus_footer_text', array(
+      'default' => __('All right reserved © 2023', 'nexus'),
+      'sanitize_callback' => 'sanitize_text_field',
+    ));
+    $wp_customize->add_control('nexus_footer_text', array(
+      'label' => __('Footer Text', 'nexus'),
+      'section' => 'nexus_footer',
+      'control' => 'nexus_footer_text',
+      'type' => 'text',
+    ));
+    //footer copyright text customize end
+
+
+}
+add_action('customize_register', 'nexus_customize_register' );
 // Theme Custom Logo
 function nexus_custom_logo_setup() {
 	$defaults = array(
@@ -38,8 +74,6 @@ function nexus_custom_logo_setup() {
 }
 add_action( 'after_setup_theme', 'nexus_custom_logo_setup' );
 
-
-// Nav Menu Register
 
 // Menu Register
 register_nav_menu( 'main_menu', __('Main Menu', 'nexus') );
